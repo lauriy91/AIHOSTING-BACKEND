2c1e23ff39263a85b218703cf2e27b661e7d2352
Laura Rodriguez King <90001118+lauriy91@users.noreply.github.com>
2021-11-28 17:37:27 -0500

    adapting admin

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@
 const express = require('express');
 //Importar mongoose
 const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const morgan = require('morgan');
+const bodyparser = require ('body-parser');
+// require('./urlDatabase')
 //Importamos la conexión a la BD
 const DatabaseConnection = require("./database/databaseConnection")
 //Importamos las rutas
 const RouterUsuario = require('./routers/usuarioRouter')
 const RouterReserva = require('./routers/reservaRouter')
 const RouterAlojamiento = require('./routers/alojamientoRouter')
-const cors = require('cors')
-
+const RouterAdmin = require('./routers/adminRouter')
 
 //Levantamos el servidor
 class Server{
@@ -22,7 +26,6 @@ class Server{
         this.app = express();
         //Configuramos parametros de express para levantar el servidor
         this.config();
-
     }
 
     config(){
@@ -31,13 +34,16 @@ class Server{
         this.app.set('port', process.env.PORT || 3000);
         //Indicar el uso del formato json para enviar informacion con JSON
         this.app.use(express.json());
-        this.app.use(cors())
+        this.app.use(morgan('dev'));
+        this.app.use(bodyparser.urlencoded({extended:true}));
+        this.app.use(bodyparser.json());
+        this.app.use(cors({origen: '*'}));
+
         //Configurar ruta raíz
         let router = express.Router();
         router.get('/', (req, res) => {
             console.log("¡Conexión exitosa!");
             res.status(200).json({message: "¡Conexión exitosa!"});
-
         });
 
         /*** CREAR RUTAS ***/
@@ -46,6 +52,7 @@ class Server{
         const alojamientoR = new RouterAlojamiento();
         const usuarioR = new RouterUsuario();
         const reservaR = new RouterReserva();
+        const adminR = new RouterAdmin();
         
         //Añadir la rutas al servidor El servidor utiliza la ruta que se creó previamente
         this.app.use('/public', express.static(`${__dirname}/imagenes`))
@@ -53,6 +60,7 @@ class Server{
         this.app.use(alojamientoR.router);
         this.app.use(reservaR.router);
         this.app.use(usuarioR.router);
+        this.app.use(adminR.router);
         
         //Levantar el servidor y ponerlo a la escucha
         this.app.listen( this.app.get('port'), ()=>{
